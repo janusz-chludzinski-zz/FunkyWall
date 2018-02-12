275a9aca88c45c077526df5b617510a51e62b076
gianni <chludzinski.janusz@gmail.com>
2018-02-12 18:22:15 +0100

    Init

---
+++ assets/js/carousel-start.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    margin:0,
+    loop:true,
+    items:1,
+    nav: true,
+    navText: ['<i class="fas fa-chevron-circle-left nav-item"></i>', '<i class="fas fa-chevron-circle-right nav-item"></i>']
+  });
+});
---
+++ assets/js/products.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+  var product = $(".product-nav");
+  
+  var posters = $(".posters");
+  var measures = $(".measures");
+  var stickers = $(".stickers");
+
+  product.on('click', function(){
+
+      if($(this).attr('data') === "poster") {
+        adjustClasses($(this));
+        $('.posters').removeClass("hidden");
+      }
+      else if($(this).attr('data') === 'measure') {
+        adjustClasses($(this));
+        $('.measures').removeClass("hidden");
+      }
+      else if($(this).attr('data') === 'sticker') {
+        adjustClasses($(this));
+        $('.stickers').removeClass("hidden");
+      }
+  })
+});
+
+function adjustClasses(target) {
+  $('.products').addClass("hidden");
+  $('.product-nav').removeClass('active');
+
+  $(target).addClass('active');
+}
